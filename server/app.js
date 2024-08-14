446db9aae41076c415069803b552e68c7c767fc3
zeru0429 <mihiretut21@gmail.com>
2024-08-14 04:11:21 -0400

    ip geter function added

--- server/app.js
+++ server/app.js
@@ -1,38 +1,49 @@
-const express = require('express');
-const cors = require('cors');
-const appRoutes = require('./routes/index');
-const path = require('path');
+const express = require("express");
+const os = require("os");
+const cors = require("cors");
+const appRoutes = require("./routes/index");
+const path = require("path");
 
 // configet dotenv
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const host = process.env.SERVER_HOST;
 const port = process.env.SERVER_PORT;
 
-
 const server = express();
 
 // middle ware  to use json
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 // cors
-server.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+server.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // server.use(cors({origin: '*',credentials: true}));
-server.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+server.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //test route
-server.get('/',async (req,res)=>{
-   res.send("test url")
+server.get("/", async (req, res) => {
+  res.send("test url");
 });
 
 //main route
 server.use(appRoutes);
 
-
-server.listen(port,host,(err)=>{
-   if(err) console.log(err);
-   console.log(`server is runing on http://${host}:${port}`);
-
+server.listen(port, getCurrentIPAddress(), (err) => {
+  if (err) console.log(err);
+  console.log(`server is runing on http://${getCurrentIPAddress()}:${port}`);
 });
 
+function getCurrentIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (let interfaceName in interfaces) {
+    const interfaceInfo = interfaces[interfaceName];
+    for (let i = 0; i < interfaceInfo.length; i++) {
+      const alias = interfaceInfo[i];
+      if (alias.family === "IPv4" && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+  return "Unable to determine IP address";
+}
